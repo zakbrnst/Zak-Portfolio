a675e6e9be7378244e20baae3df94f40f5a6464d
zakbrnst <zakbrnst@icloud.com>
2025-09-22 13:17:49 +0100

    Contact page started

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -3,19 +3,18 @@ import './Contact.css'
 import Header from './header'
 
 function Contact() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div className="navBar">
         <Header />
       </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <h1>Contact me</h1>
-      </div>
+        <div className="cardContainer">
+          <div className="contactCard">
+            <h2>Email</h2>
+          </div>
+        </div>
     </>
   )
 }
